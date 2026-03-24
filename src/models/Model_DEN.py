Refactor Model_DEN and Model_MLP by removing unused MultiHeadClassifier; enhance DENLayer dimensions; update LwFPlugin for correct softmax and log_softmax name usage in distillation loss calculation.

src/models/Model_DEN.py
@@ -1,40 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import avalanche.models as models
-from avalanche.models import (
-    MultiHeadClassifier,
-    IncrementalClassifier,
-)
+from avalanche.models import IncrementalClassifier
 from .layers.DENLayer import DENLayer
 from .layers.LinearAttention import LinearAttention
-
-
-class Model_DEN_TIL(models.MultiTaskModule):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.den_1 = DENLayer((28 * 28), 80)
-        self.linear_1 = nn.Linear(self.den_1.out_features, 400)
-        self.classifier = MultiHeadClassifier(self.linear_1.out_features, 2)
-
-    def adaptation(self, experience):
-        super().adaptation(experience)
-        self.classifier.adaptation(experience=experience)
-
-    def forward(self, x, task_labels):
-        x = x.reshape((x.size(0), -1))
-        x = F.relu(self.den_1(x))
-        x = F.relu(self.linear_1(x))
-        x = self.classifier(x, task_labels)
-        return x
 
 
 class Model_DEN_CIL(models.DynamicModule):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.den_1 = DENLayer(28 * 28, 80)
-        self.den_2 = DENLayer(self.den_1.out_features, 80)
+        self.den_1 = DENLayer(28 * 28, 400)
+        self.den_2 = DENLayer(self.den_1.out_features, 400)
         self.classifier = IncrementalClassifier(self.den_2.out_features)
 
     def adaptation(self, experience):

src/models/Model_MLP.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import avalanche.models as models
-from avalanche.models import IncrementalClassifier, MultiHeadClassifier
+from avalanche.models import IncrementalClassifier
 from .layers.LinearAttention import LinearAttention
 
 
@@ -61,25 +61,6 @@ class model_MLP_attention(models.DynamicModule):
         x = self.classifier(h3)
         return x
 
-
-class Model_MLP_TIL(models.MultiTaskModule):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.linear_1 = nn.Linear(28 * 28, 400)
-        self.linear_2 = nn.Linear(self.linear_1.out_features, 400)
-        self.classifier = MultiHeadClassifier(self.linear_2.out_features, 2)
-
-    def adaptation(self, experience):
-        super().adaptation(experience)
-        self.classifier.adaptation(experience=experience)
-
-    def forward(self, x, task_labels):
-        x = x.reshape((x.size(0), -1))
-        x = F.relu(self.linear_1(x))
-        x = F.relu(self.linear_2(x))
-        x = self.classifier(x, task_labels)
-        return x
 
 
 class Model_MLP_Cifar(models.DynamicModule):

src/plugins/LwFPlugin.py
@@ -49,14 +49,14 @@ class LwFPlugin(SupervisedPlugin):
     def _distillation_loss(self, out_s, out_t):
         T = self.temperature
 
-        p = F.softmax(out_t / T, dim=1)
-        q = F.log_softmax(out_s / T, dim=1)
+        q = F.softmax(out_t / T, dim=1)
+        log_p = F.log_softmax(out_s / T, dim=1)
 
         if self.active_units is not None:
-            p_old = p[:, self.active_units]
-            q_old = q[:, self.active_units]
+            q = q[:, self.active_units]
+            log_p = log_p[:, self.active_units]
 
         else:
-            p_old, q_old = p, q
+            q, log_p = q, log_p
 
-        return F.kl_div(q_old, p_old, reduction="batchmean") * (T**2)
+        return F.kl_div(log_p, q, reduction="batchmean") * (T**2)